5071eaaa6c3dbc2e3b9082485754c6aad411384a
taesiri <taesiri@live.com>
2014-05-16 20:14:57 +0430

    Project three Updated

--- app/js/projects/pthree.js
+++ app/js/projects/pthree.js
@@ -1,24 +1,52 @@
-var board;
 
-function drawGraphFunc() {
-  board = JXG.JSXGraph.initBoard('box', {boundingbox: [-10, 10, 10, -10], axis:true});
-  board.create('functiongraph', [function(x){return staticWilkinson(x);}, -10,10]);
-}
+function createSensivityTable() {
+  var tbody = generateTableBody('sensivityTable', ['#', 'Original Polynomial', 'Modified Polynomail']);
+  var data = new Array();
 
-function staticWilkinson(x) {
-  return (x-1)*(x-2)*(x-3)*(x-4)*(x-5)*(x-6)*(x-7)*(x-8)*(x-9)*(x-10);
-}
+  var mroots = "20.24021159+0.j,18.98804416+1.0812886j,18.98804416-1.0812886j,16.64490643+1.74743755j,16.64490643-1.74743755j,14.21576452+1.57479827j,14.21576452-1.57479827j,12.14115215+0.88792082j,12.14115215-0.88792082j,10.71709178+0.j,10.06671308+0.j,8.99645948+0.j,7.99970563+0.j,7.00009441+0.j,5.99998864+0.j,5.00000094+0.j,3.99999994+0.j,3.00000000+0.j,2.00000000+0.j,1.00000000+0.j".split(',');
 
-function wilkinsonBuilder(x, v) {
-  if ( v == 1) {
-    return x-1;
-  } else if ( v > 0) {
-    return (x-v)*wilkinsonBuilder(x, v-1);
+  for (var i = 0; i < mroots.length; i ++) {
+    data[i] = {id : i+1, or:i+1, mr: mroots[mroots.length - i -1] };
   }
-  return NaN;
+
+  fillTable(tbody, data);
 }
 
-function wilkinson(x) {
-  return wilkinsonBuilder(x,6);
+function generateTableBody(tableName, labels) {
+  tbody = document.getElementById(tableName);
+
+  theaderrow = document.createElement("TR");
+
+  while(theaderrow.firstChild) {
+    theaderrow.removeChild(theaderrow.firstChild);
+  }
+
+  labels.forEach(function (entry) {
+    tCell = document.createElement("TH");
+    hNode = document.createTextNode(entry);
+    tCell.appendChild(hNode);
+    theaderrow.appendChild(tCell);
+  });
+
+  while(tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  tbody.appendChild(theaderrow);
+
+  return tbody;
 }
 
+function fillTable(tb, data) {
+  for(entry in data){
+    row = document.createElement("TR");
+    for ( key in data[entry]) {
+      cell = document.createElement("TD");
+      tNode = document.createTextNode(data[entry][key]);
+      cell.appendChild(tNode);
+
+      row.appendChild(cell);
+    }
+    tb.appendChild(row);
+  }
+}
--- python/pthree.py
+++ python/pthree.py
@@ -1,17 +1,18 @@
 # Third Project Using Python 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 N = 20
 p = np.array([1.0, -1.0], dtype = float)
 for n in range(2,N+1):
     p = np.append(p, [0.0]) - np.append([0.0], p*n)
 
-x = np.arange(1,21)
+print p
+x = np.arange(1,21,dtype=float)
 print x
 z = np.polyval(p, x)
 print z
-
 
 xx = np.arange(0.5, 20.5, 0.05, dtype=float)
 yy = np.zeros_like(xx)
@@ -20,7 +21,34 @@ for i in range(xx.size):
     yy[i] = np.sign(y)*np.log(1.0 + abs(y))
 
 zz = np.zeros_like(xx)
-plt.plot(xx,yy, xx,zz)
-#title('Log of Wilkinson''s polynomial')
-#xlabel('$x$')
+
+r = np.roots(p)
+print '\nOriginal Wlkinson''s Roots:\n'
+print r
+
+# Let's do modification !
+
+p[1] = p[1] - 10**-8
+
+rm = np.roots(p)
+print '\nModified Wilkinson''s Roots:\n'
+print rm
+
+xxm = np.arange(0.5, 20.5, 0.05, dtype=float)
+yym = np.zeros_like(xxm)
+
+for i in range(xxm.size):
+    y = np.polyval(p,xxm[i])
+    yym[i] = np.sign(y)*np.log(1.0 + abs(y))
+
+zzm = np.zeros_like(xxm)
+
+fig, ax = plt.subplots()
+ax.plot(xx,yy , label='Wilkinson''s Polynomial')
+ax.plot(xxm,yym , label='Modified - $C_{19} \\rightarrow C_{19} - 10^{-8}$')
+ax.legend(loc='upper left')
+
+
 plt.show()
+
+
